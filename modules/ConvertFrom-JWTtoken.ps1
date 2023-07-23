cad304063af196d1dddad64ce69e545705cb117c
Fabian Bader <fabian.bader@toolsection.info>
2023-07-23 14:51:30 +0200

    Use single files for helper functions

--- modules/ConvertFrom-JWTtoken.ps1
+++ modules/ConvertFrom-JWTtoken.ps1
@@ -1,17 +1,3 @@
-# Helper Functions
-function Get-TenantID {
-    [cmdletbinding()]
-    Param(
-        [Parameter(ParameterSetName = 'Domain', Mandatory = $True)]
-        [String]$domain
-    )
-    Process {
-        $openIdConfig = Invoke-RestMethod "https://login.microsoftonline.com/$domain/.well-known/openid-configuration"
-        $TenantId = $OpenIdConfig.authorization_endpoint.Split("/")[3]
-        return $TenantId
-    }
-}
-
 function ConvertFrom-JWTtoken {
     <#
     .DESCRIPTION
---
+++ modules/Get-TenantId.ps1
@@ -0,0 +1,12 @@
+function Get-TenantID {
+    [cmdletbinding()]
+    Param(
+        [Parameter(ParameterSetName = 'Domain', Mandatory = $True)]
+        [String]$domain
+    )
+    Process {
+        $openIdConfig = Invoke-RestMethod "https://login.microsoftonline.com/$domain/.well-known/openid-configuration"
+        $TenantId = $OpenIdConfig.authorization_endpoint.Split("/")[3]
+        return $TenantId
+    }
+}
